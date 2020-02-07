8608f792e42b3b2d4bec05103df07efe3f55cd4e
KurisuLim <kazuki1002003@yahoo.com>
2020-02-07 15:42:06 -0800

    creating basic class and object

---
+++ src/week/three/User.java
@@ -0,0 +1,10 @@
+package week.three;
+
+public class User {
+
+    String firstName;
+    String lastName;
+    String jobTitle;
+    String email;
+    int id;
+}
---
+++ src/week/three/Users.java
@@ -0,0 +1,8 @@
+package week.three;
+
+public class Users {
+    public static void main(String[] args ){
+        User user = new User();
+
+    }
+}
