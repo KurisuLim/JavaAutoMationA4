2a5f6af72fd2598d9e434bd1551db0cad334fb9a
KurisuLim <kazuki1002003@yahoo.com>
2020-02-06 22:53:08 -0800

    adds try and catch example

---
+++ src/week/two/ExceptionHandling.java
@@ -0,0 +1,25 @@
+package week.two;
+
+import java.util.Scanner;
+
+public class ExceptionHandling {
+    public static void main(String[] args ){
+        Scanner input = new Scanner(System.in);
+        int x = 1;
+        do {
+            try {
+                System.out.println("Enter first number: ");
+                int num1 = input.nextInt();
+                System.out.println("Enter second number: ");
+                int num2 = input.nextInt();
+
+                int sum = num1 + num2;
+                System.out.println(sum);
+                x = 2;
+
+            } catch (Exception e) {
+                System.out.println(("Error: You need to enter a number!"));
+            }
+        }while (x == 1);
+    }
+}
