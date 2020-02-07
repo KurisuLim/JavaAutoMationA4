749edaa1f196535c386015ce5b4c9ff190f04105
KurisuLim <kazuki1002003@yahoo.com>
2020-02-07 13:54:45 -0800

    adds first example of a class and object

---
+++ src/week/three/ObjectOrientedProgramming.java
@@ -0,0 +1,11 @@
+package week.three;
+
+public class ObjectOrientedProgramming {
+    String name = "Chris";
+    int age = 33;
+    public static void main(String[] args) {
+        ObjectOrientedProgramming myInfo = new ObjectOrientedProgramming();
+        System.out.println("My name is: " + myInfo.name);
+        System.out.println("My age is: " + myInfo.age);
+    }
+}
