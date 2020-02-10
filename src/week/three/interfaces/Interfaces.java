61d2acaa26eb3bb6c16de6d77e07e7c333e8057e
KurisuLim <kazuki1002003@yahoo.com>
2020-02-09 22:42:40 -0800

    adds enum example

--- src/week/three/interfaces/Interfaces.java
+++ src/week/three/interfaces/Interfaces.java
@@ -3,6 +3,16 @@ import java.io.*;
 
 public class Interfaces {
 
+    enum Actions {
+        ATTACK,
+        DEFEND,
+        WALK,
+        PICKUP,
+        OPEN,
+        EQUIP,
+        USE
+    }
+
     public static void main(String[] args){
         // creating an instance of a warrior
         Warrior warriorD = new Warrior();
@@ -10,7 +20,10 @@ public class Interfaces {
         warriorD.manaPoints(5);
         warriorD.atkPoints(7);
         warriorD.defPoints(5);
-
         warriorD.printStats();
+
+        Actions action = Actions.ATTACK;
+        System.out.println("\nWarrior Class just " + action + " a monster.");
+
     }
 }
