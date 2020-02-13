6fa5e6971e964a57c34ff92c16aa51cca97abc2f
KurisuLim <kazuki1002003@yahoo.com>
2020-02-12 20:51:55 -0800

    adds array list methods example

--- src/week/four/ArrayListExample.java
+++ src/week/four/ArrayListExample.java
@@ -1,4 +1,5 @@
 package week.four;
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
@@ -17,5 +18,24 @@ public class ArrayListExample {
         for(String list : users) {
             System.out.println(list);
         }
+        //add method adds a new element on the list
+        users.add("Test Pro");
+        System.out.println(users); // [Chris, Marianne, CJ, Arianne, Test Pro]
+
+        //get methods display what index
+        System.out.println(users.get(4)); // display Test Pro
+
+        //set methods grabs the element and replace or add new value
+        users.set(4,"Test Pro Rocks!");
+        System.out.println(users); // [Chris, Marianne, CJ, Arianne, Test Pro Rocks!]
+
+        //size methods returns the size of the array list
+        System.out.println(users.size()); // 5
+
+        //remove method removes the element and value
+        users.remove(4);
+        System.out.println(users); // [Chris, Marianne, CJ, Arianne]
+
     }
+
 }
