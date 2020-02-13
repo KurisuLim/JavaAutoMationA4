49334307b9d2e4f9f27cee386ed747d9b05c1807
KurisuLim <kazuki1002003@yahoo.com>
2020-02-12 21:20:54 -0800

    using reverse and sort method as an example

--- src/week/four/ConvertListToArray.java
+++ src/week/four/ConvertListToArray.java
@@ -1,6 +1,7 @@
 package week.four;
 
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 public class ConvertListToArray {
@@ -12,6 +13,15 @@ public class ConvertListToArray {
             role[i] = users.get(i);
         }
 
-        System.out.println(Arrays.toString(role));
+        System.out.println(Arrays.toString(role)); //[Account Owner, Account Admin, Facilitator, Coach]
+
+        //Sorting the list
+        Collections.sort(users);
+        System.out.println(users); // [Account Admin, Account Owner, Coach, Facilitator]
+
+        //reversing the list
+        Collections.reverse(users);
+        System.out.println(users); //[Facilitator, Coach, Account Owner, Account Admin]
+
     }
 }
